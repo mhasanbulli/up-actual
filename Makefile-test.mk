4fc2af2baad36b871313fc8b852392f73645d63b
Mustafa Hasanbulli <mustafa@hasanbul.li>
2024-12-11 21:24:47 +1300

    Add a simple test

---
+++ Makefile-test.mk
@@ -0,0 +1,15 @@
+## format, lint and type check
+check: export SKIP=test
+check: hooks
+
+## format and lint
+format: export SKIP=pyright,test
+format: hooks
+
+## pyright type check
+pyright: $(venv)
+	uv run pyright
+
+## run tests
+test: $(venv)
+	uv run pytest
