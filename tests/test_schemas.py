ceac8165f719abb6c73b0a59754dc629776150f5
Mustafa Hasanbulli <mustafa@hasanbul.li>
2025-02-02 10:21:01 +1100

    Add validator test

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -6,6 +6,7 @@ from up.classes import UpAPI
 from up.utils import load_json, schema_validator
 
 ACCOUNTS_SCHEMA = load_json(Path(__file__).parent.parent / "schemas/accounts.schema.json")
+TRANSACTIONS_SCHEMA = load_json(Path(__file__).parent.parent / "schemas/transactions.schema.json")
 
 
 @pytest.mark.schema
@@ -14,3 +15,15 @@ def test_accounts_schema():
     response = accounts_endpoint.get_endpoint_response()
 
     schema_validator(json.loads(response.text), ACCOUNTS_SCHEMA)
+
+
+@pytest.mark.schema
+def test_transactions_schema():
+    transactions_endpoint = UpAPI(endpoint="transactions")
+    url_params = {
+        "page[size]": 1,
+    }
+
+    response = transactions_endpoint.get_endpoint_response(url_params=url_params)
+
+    schema_validator(json.loads(response.text), TRANSACTIONS_SCHEMA)
--- up/classes.py
+++ up/classes.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from urllib.parse import urlencode, urljoin
 
 import requests
 from requests import Response
@@ -18,14 +19,30 @@ class UpAPI:
     def headers(self) -> dict:
         return {"Authorization": f"Bearer {get_token()}"}
 
-    def get_url(self) -> str:
-        return f"{self.up_api_url}/{self.endpoint}"
+    def get_url(self, url_params: dict | None = None) -> str | bytes:
+        if url_params is None or not self.endpoint:
+            return self.up_api_url
 
-    def get_endpoint_response(self) -> Response:
+        base_url = f"{self.up_api_url}/{self.endpoint}"
+
+        params = {}
+
+        for key, value in url_params.items():
+            if isinstance(value, list):
+                for item in value:
+                    params[key] = item
+            elif value is not None:
+                params[key] = value
+
+        encoded_params = urlencode(params)
+
+        return urljoin(base=base_url, url="?" + encoded_params)
+
+    def get_endpoint_response(self, url_params: dict | None = None) -> Response:
         """
         For a given `endpoint`, it returns the response from the Up API.
         """
-        url = self.get_url()
+        url = self.get_url(url_params)
         response = requests.get(url=url, headers=self.headers, timeout=1)
 
         return response
