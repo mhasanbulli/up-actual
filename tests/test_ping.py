b792f7a3ff0a6f6d2de5ae1df30661ae4180a1a3
Mustafa Hasanbulli <mustafa@hasanbul.li>
2025-02-02 10:39:38 +1100

    Add a dummy test

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -2,6 +2,10 @@ import pytest
 from up.classes import UpAPI
 
 
+def test_dummy_test():
+    assert 1 == 1
+
+
 @pytest.mark.integration
 def test_ping():
     ping_endpoint = UpAPI(endpoint="util/ping")
